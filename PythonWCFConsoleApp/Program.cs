78ce539438728ceccb51b46366f3fee6cda44a68
RF-Team <rfteam@gojisolutions.com>
2020-06-02 09:17:16 +0300

    update

--- PythonWCFConsoleApp/Program.cs
+++ PythonWCFConsoleApp/Program.cs
@@ -1,4 +1,6 @@
-﻿using PythonWCFShared;
+﻿using Microsoft.Win32;
+using PythonWCFShared;
+using RegistryClassApi;
 using System;
  
  
@@ -12,7 +14,19 @@ namespace PythonWCFConsoleApp
 
             try
             {
-                WCFConnect.OpenService("localhost" , 8022);                
+
+                string ipAddress = "";
+                const string ServerRegistry = "SOFTWARE\\Goji Solutions\\Harmoni";
+                clsRegistry reg = new clsRegistry();
+                ipAddress = reg.GetStringValue(Registry.LocalMachine, ServerRegistry, "LocalIpAddress");
+                if (ipAddress == null)
+                {
+                    Console.WriteLine("Failed to get the resgitry key for local ip address");
+                }
+                else
+                {
+                    WCFConnect.OpenService(ipAddress, 8022);
+                }
             }
             catch (Exception err)
             {
--- PythonWCFHttpWinService/Service1.cs
+++ PythonWCFHttpWinService/Service1.cs
@@ -1,4 +1,6 @@
-﻿using PythonWCFShared;
+﻿using Microsoft.Win32;
+using PythonWCFShared;
+using RegistryClassApi;
 using System;
  
 using System.ServiceProcess;
@@ -17,7 +19,19 @@ namespace PythonWCFHttpWinService
         {
             try
             {
-                WCFConnect.OpenService("localhost" , 8020);                
+                string ipAddress = "";
+                const string ServerRegistry = "SOFTWARE\\Goji Solutions\\Harmoni";
+                clsRegistry reg = new clsRegistry();
+                ipAddress = reg.GetStringValue(Registry.LocalMachine, ServerRegistry, "LocalIpAddress");
+                if (ipAddress == null)
+                {
+                    Console.WriteLine("Failed to get the resgitry key for local ip address");
+                }
+                else
+                {
+                    WCFConnect.OpenService(ipAddress, 8022);
+                } 
+                            
             }
             catch (Exception err)
             {
